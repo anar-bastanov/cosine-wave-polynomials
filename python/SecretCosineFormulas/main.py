8610ab86a5cc4480083d79f1ae63479625760f42
Anar Bastanov <69395109+anar-bastanov@users.noreply.github.com>
2024-08-11 23:55:09 +0400

    Remove semicolons

--- python/SecretCosineFormulas/main.py
+++ python/SecretCosineFormulas/main.py
@@ -3,7 +3,7 @@ from formula_ctor import FormulaConstructor
 
 
 def main():
-    print("Enter an integer multiplier to substitute for `n` in `cos(n * x)`:");
+    print("Enter an integer multiplier to substitute for `n` in `cos(n * x)`:")
 
     while True:
         try:
@@ -14,7 +14,7 @@ def main():
         except EOFError:
             exit()
 
-    print("Specify the format for the output polynomial (`Cosine Only` or `Sine And Cosine):");
+    print("Specify the format for the output polynomial (`Cosine Only` or `Sine And Cosine):")
 
     while True:
         try:
@@ -32,11 +32,11 @@ def main():
             f"The input exceeded the limit of {FormulaConstructor.max_multiplier} for the multiplier. The developer " +
             f"intentionally put this restriction as outputs from these functions can grow rapidly and consume too much " +
             f"memory. If the machine you are running this code on is powerful enough and you still want to try higher " +
-            f"values, you can always inspect the code and remove the limit manually.");
-        return;
+            f"values, you can always inspect the code and remove the limit manually.")
+        return
 
-    print("Output formula:");
-    print(formula);
+    print("Output formula:")
+    print(formula)
 
 
 if __name__ == '__main__':
